8631b9c1aa0a9ed9c008aed45c892d318f55739c
md-fazle <rf.fazle.rabbi@gmail.com>
2025-08-28 20:37:52 +0600

    3rd => LineQ

--- TheTestOfficeProject/Program.cs
+++ TheTestOfficeProject/Program.cs
@@ -10,17 +10,38 @@ namespace TheTestOfficeProject
         static void Main(string[] args)
         {
 
-             List<Employee> employeeList = Data.GetEmployees();
+            // List<Employee> employeeList = Data.GetEmployees();
 
-             var filteredEmployees = employeeList.Where(e => e.isActive && e.salary > 55000).ToList();
+            // var filteredEmployees = employeeList.Where(e => e.isActive && e.salary > 55000).ToList();
 
-            foreach (var emp in filteredEmployees)
+            //foreach (var emp in filteredEmployees)
+            //{
+            //    Console.WriteLine($"ID: {emp.Id}, Name: {emp.name}, Department: {emp.department}, Designation: {emp.Designation}, isActive: {emp.isActive}, Salary: {emp.salary}");
+            //}
+            //Console.ReadLine(); 
+
+            List<Employee> employeeList = Data.GetEmployees();
+            List<Department> dept = Data.GetDepartments();
+
+            var result = from e in employeeList
+                         join d in dept on e.deptId equals d.DeptID
+                         where e.isActive && e.salary > 50000
+                         select new
+                         {
+                             e.Id,
+                             e.name,
+                             e.department,
+                             e.Designation,
+                             e.isActive,
+                             e.salary,
+                             d.DeptName
+                         };
+            foreach (var emp in result)
             {
-                Console.WriteLine($"ID: {emp.Id}, Name: {emp.name}, Department: {emp.department}, Designation: {emp.Designation}, isActive: {emp.isActive}, Salary: {emp.salary}");
+                Console.WriteLine($"ID: {emp.Id}, Name: {emp.name}, Department: {emp.department}, Designation: {emp.Designation}, isActive: {emp.isActive}, Salary: {emp.salary}, DeptName: {emp.DeptName}");
             }
-            Console.ReadLine(); 
+            Console.ReadLine();
 
-           
             //List<Department> dept = Data.GetDepartments();
 
             //var deptFiltered = dept.Filter(d => d.DeptName.Contains("IT"));
