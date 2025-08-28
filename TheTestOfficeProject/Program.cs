f5321eeb73c7c869a1e53ef62459fea4762fb7b6
md-fazle <rf.fazle.rabbi@gmail.com>
2025-08-28 20:07:43 +0600

    First-> output about employeeList

--- TheTestOfficeProject/Program.cs
+++ TheTestOfficeProject/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TCPData;
+using TCPExtension;
+
+namespace TheTestOfficeProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+             List<Employee> employeeList = Data.GetEmployees();
+
+             var filteredEmployees = employeeList.Filter(e => e.isActive == true);
+
+            foreach(var emp in filteredEmployees)
+            {
+                Console.WriteLine($"ID: {emp.Id}, Name: {emp.name}, Department: {emp.department}, Designation: {emp.Designation}, isActive: {emp.isActive}, Salary: {emp.salary}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
