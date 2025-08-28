054b866e298ce573f107d6d835ec1889677a5368
md-fazle <rf.fazle.rabbi@gmail.com>
2025-08-28 20:27:21 +0600

    2nd=> LINQ

--- TheTestOfficeProject/Program.cs
+++ TheTestOfficeProject/Program.cs
@@ -1,4 +1,6 @@
-﻿using TCPData;
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using TCPData;
 using TCPExtension;
 
 namespace TheTestOfficeProject
@@ -10,13 +12,24 @@ namespace TheTestOfficeProject
 
              List<Employee> employeeList = Data.GetEmployees();
 
-             var filteredEmployees = employeeList.Filter(e => e.isActive == true);
+             var filteredEmployees = employeeList.Where(e => e.isActive && e.salary > 55000).ToList();
 
-            foreach(var emp in filteredEmployees)
+            foreach (var emp in filteredEmployees)
             {
                 Console.WriteLine($"ID: {emp.Id}, Name: {emp.name}, Department: {emp.department}, Designation: {emp.Designation}, isActive: {emp.isActive}, Salary: {emp.salary}");
             }
             Console.ReadLine(); 
+
+           
+            //List<Department> dept = Data.GetDepartments();
+
+            //var deptFiltered = dept.Filter(d => d.DeptName.Contains("IT"));
+
+            //foreach (var d in deptFiltered)
+            //{
+            //    Console.WriteLine($"DeptID: {d.DeptID}, DeptName: {d.DeptName}");
+            //}
+            //Console.ReadLine();
             // this is okay
         }
     }
